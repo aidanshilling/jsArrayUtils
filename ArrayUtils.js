eece3a23330d0017a9d3ced7099acadd83220ea4
aidanshilling <aidanshilling91@gmail.com>
2019-10-14 14:07:46 -0400

    Refined and added a few more functions.

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -1,4 +1,5 @@
 function printReverse(arry) {
+//Prints a given array in reverse order to console
     var length = arry.length;
     for(var i = length; i >= 0; i--) {
         console.log(arry[i]);
@@ -6,15 +7,33 @@ function printReverse(arry) {
 }
 
 function isUniform(arry) {
-    var count = 1;
+//Returns if given array is uniform (all values are the same)
     var itemOne = arry[0];
-
-    while(count < arry.length) {
+    for(var i = 0; i < arry.length; i++) {
         if(itemOne !== arry[count]) {
             console.log("Failed on index: " + count);
             return false
         }
-        count++;
     }
     return true;
+}
+
+function sumArray(arry) {
+//Sums all the values in a given array
+    var sum = 0;
+    arry.forEach(function(item){
+        sum += item;
+    });
+    return sum;
+}
+
+function max(arry) {
+//Returns maximum value in a given array
+    var max = 0;
+    arry.forEach(function(item){
+        if (item > max) {
+            max = item;
+        }
+    });
+    return max;
 }

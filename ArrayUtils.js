9861be0e2f7a356a0ca90ba0650e25d5d9dbccc0
aidanshilling <aidanshilling91@gmail.com>
2019-10-15 09:48:33 -0400

    Added array.insert

--- ArrayUtils.js
+++ ArrayUtils.js
@@ -37,3 +37,17 @@ Array.prototype.max = function() {
     });
     return max;
 }
+
+Array.prototype.insert = function(item, index) {
+//Inserts a value to a certain index in an array and pushed back everything after that index
+//Function is kinda useless because splice exists
+    var arry = [];
+    for(var i = index; i < this.length; i++) {
+        arry.push(this[i]);
+    }
+    this.splice(index);
+    this[index] = item;
+    for(var i = 0; i < arry.length; i++) {
+        this.push(arry[i]);
+    }
+}
